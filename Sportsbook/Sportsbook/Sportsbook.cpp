593fd13433c26d3bd4845c0e8ce1418091e7f85b
TheoProgramming <bikosenter@gmail.com>
2017-02-10 20:56:27 +0200

    -Registration

--- Sportsbook/Sportsbook/Sportsbook.cpp
+++ Sportsbook/Sportsbook/Sportsbook.cpp
@@ -5,11 +5,29 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-	cout << "this is sportsbook" << endl;
-	int i = 0;
-	cout << i << endl; 
+	//for starters we are not going to use the preprocessing #ifdef, but remain in a known environment of argv,argc
+	char parameter = atof(argv[0]);
+	// Eggrafi part
+	if (parameter == 'R') {
+		bool registration_flag = true;
+		string username, password, appuser;
+		cout << "<username>" << endl << "<password>" << endl;
+		cin >> username;
+		cout << endl;
+		cin >> password;
+		int offset;
+		fstream file("registrations.csv");
+		while (!file.eof()) {
+			getline(file, appuser);
+			if ((offset = appuser.find(username, 0)) != string::npos) {
+				cout << "There is already a register with that username." << endl;
+				return 0;
+			}
+		}
+	}
+	// end of eggrafi!
     return 0;
 }
 
