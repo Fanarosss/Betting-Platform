48caab3c3fdbb59df32d36c979864a7ed0e665b3
TheoProgramming <bikosenter@gmail.com>
2017-02-10 21:16:31 +0200

    -Login

--- Sportsbook/Sportsbook/Sportsbook.cpp
+++ Sportsbook/Sportsbook/Sportsbook.cpp
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
 {
 	//for starters we are not going to use the preprocessing #ifdef, but remain in a known environment of argv,argc
 	char parameter = atof(argv[0]);
+	string username, password, appuser;
+	bool registration_flag = true;
 	// Eggrafi part
 	if (parameter == 'R') {
-		bool registration_flag = true;
-		string username, password, appuser;
 		cout << "<username>" << endl << "<password>" << endl;
 		cin >> username;
 		cout << endl;
@@ -26,8 +26,33 @@ int main(int argc, char *argv[])
 				return 0;
 			}
 		}
+		//this is the part where registration is succesfull, the user logins and his username and password are passed in csv
 	}
 	// end of eggrafi!
+	cout << "Welcome to sportsbook" << endl << "Please enter your username and password for signing in. If you don't have an account please press enter, or type guest" << endl;
+	registration_flag = false;
+	cout << "Username: ";
+	cin >> username;
+	if (username != "guest") {
+		int offset;
+		fstream file("registrations.csv");
+		while (!file.eof()) {
+			getline(file, appuser);
+			if ((offset = appuser.find(username, 0)) != string::npos) {
+				registration_flag = true;
+				break;
+			}
+		}
+		if (!registration_flag) {
+				cout << "There is no register with that username. Please try again! " << endl;
+		}
+		cout << endl << "Password: ";
+		cin >> password;
+		cout << endl;
+	}
+	else {
+		cout << "Logged in as guest" << endl;
+	}
     return 0;
 }
 
