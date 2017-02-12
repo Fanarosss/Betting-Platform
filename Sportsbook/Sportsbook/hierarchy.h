0330082cefe340b8f654e28385b1c953cc0a89bd
Κωνσταντίνος Αθηναίος <kostasa97@gmail.com>
2017-02-12 21:21:33 +0200

    hierarchy

--- Sportsbook/Sportsbook/hierarchy.h
+++ Sportsbook/Sportsbook/hierarchy.h
@@ -7,21 +7,24 @@ class Node {
 };
 
 class Category : public Node {
-
+	int id;
+	vector<Subcategory> Subcategories;
 };
 
 class Subcategory : public Category {
-
+	int id;
 };
 
-class Event {
+class Event : public Subcategory {
+	int id;
 	std::string name;
 	std::string date;
 	std::string time;
 
 };
 
-class Market {
+class Market : public Event {
+	int id;
 	Selection ** selection_menu;
 };
 
@@ -29,6 +32,7 @@ class Price {
 	double price;
 };
 
-class Selection {
+class Selection : public Market {
+	int id;
 	Price apodosi;
 };
