876a4a21296dd4e0dc19ec5c2cb87f18bce01192
TheoProgramming <bikosenter@gmail.com>
2017-02-12 12:40:20 +0200

    Category: users

--- Sportsbook/Sportsbook/Users_categories.h
+++ Sportsbook/Sportsbook/Users_categories.h
@@ -1,31 +1,37 @@
 #pragma once
 
-class Guest { 
-	protected:
-		//access by all the others too.
-	public:
-		virtual void Operation(char);
+class User {
+//abstract class
+public:
+	virtual void Operation(char)=0;
+};
+
+class Guest : User{ 
+protected:
+	//access by all the others too.
+public:
+	void Operation(char);
 
 };
 
-class Punter : public Guest{
+class Wallet {
+protected:
+	double amount;
+};
+
+class Punter : public User{
 private:
 	Wallet wallet;
 public:
 	void Operation(char);
 };
 
-class Wallet : Punter{
-protected:
-	double amount;
-};
-
-class Trader : Guest{
+class Trader : User{
 public:
 	void Operation(char);
 };
 
-class Director : Guest{
+class Director : User{
 public:
 	void Operation(char);
 };
