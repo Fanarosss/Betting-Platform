7e9c92cbddd4935606191945cd6c98097584bee6
TheoProgramming <bikosenter@gmail.com>
2017-02-13 12:54:03 +0200

    function for reading from csv

--- Sportsbook/Sportsbook/Main_functions.cpp
+++ Sportsbook/Sportsbook/Main_functions.cpp
@@ -8,3 +8,17 @@ bool check_for_password(string username) {
 	//body
 	return parameter1;
 }
+
+string read_from_csv(int my_pos,string appuser) {
+	size_t pos;
+	for (int i = 0; i<my_pos; i++) {
+		pos = appuser.find("|");
+		appuser = appuser.substr(pos + 1); //gia na aferesei kai tin pavla mazi
+	} //krataei olo to string deksia apo auto pou psaxnw
+	int count_of_string = 0;
+	while ((appuser[count_of_string] != '|')) {
+		count_of_string++;
+	}
+	appuser.erase(appuser.begin() + count_of_string, appuser.end()); //now appuser = what im looking for
+	//svistike kai to deksia
+}
--- Sportsbook/Sportsbook/Main_functions.h
+++ Sportsbook/Sportsbook/Main_functions.h
@@ -1,3 +1,5 @@
 #pragma once
 
-bool check_for_password(string username);
+bool check_for_password(string );
+
+string read_from_csv(int ,string);
